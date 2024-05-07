1a3f6d0577e098f434dcbf742dbbbba166f1680f
YevhenPetrovskyi <71007428+YevhenPetrovskyi@users.noreply.github.com>
2024-05-07 18:13:40 +0300

    HW ready

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,56 @@
-import contactsService from "../services/contactsServices.js";
+import * as contactsServices from '../services/contactsServices.js';
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  const contacts = await contactsServices.listContacts();
+  return res.json(contacts);
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+  const { id } = req.params;
 
-export const deleteContact = (req, res) => {};
+  const result = await contactsServices.getContactById(id);
 
-export const createContact = (req, res) => {};
+  if (!result) {
+    return res.status(404).json({ message: 'Not found' });
+  }
+  return res.json(result);
+};
 
-export const updateContact = (req, res) => {};
+export const deleteContact = async (req, res) => {
+  const { id } = req.params;
+
+  const result = await contactsServices.removeContact(id);
+
+  if (!result) {
+    return res.status(404).json({ message: 'Not found' });
+  }
+
+  return res.json(result);
+};
+
+export const createContact = async (req, res) => {
+  const { name, email, phone } = req.body;
+
+  const result = await contactsServices.addContact(name, email, phone);
+
+  return res.status(201).json(result);
+};
+
+export const updateContact = async (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    return res
+      .status(400)
+      .json({ message: 'Body must have at least one field' });
+  }
+
+  const { id } = req.params;
+  const body = req.body;
+
+  const result = await contactsServices.updateContact(id, body);
+
+  if (!result) {
+    return res.status(404).json({ message: 'Not found' });
+  }
+
+  return res.json(result);
+};
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,28 @@
-import Joi from "joi";
+import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-
-})
+  name: Joi.string().alphanum().min(3).max(30).required(),
+  email: Joi.string().email({ minDomainSegments: 2 }).required(),
+  phone: Joi.string()
+    .regex(
+      /^\+?(\d{2}-?\d{3}-?\d{3}-?\d{2}-?\d{2})$|^\d{7}$|^\d{10}$|^\+?\d{12}$/
+    )
+    .message(
+      'Number must have 7 numbers or 7 numbers and codes, like 3801234567 or 1234567'
+    )
+    .required(),
+});
 
 export const updateContactSchema = Joi.object({
-
-})
+  name: Joi.string().alphanum().min(3).max(30),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+  }),
+  phone: Joi.string()
+    .regex(
+      /^\+?(\d{2}-?\d{3}-?\d{3}-?\d{2}-?\d{2})$|^\d{7}$|^\d{10}$|^\+?\d{12}$/
+    )
+    .message(
+      'Number must have 7 numbers or 7 numbers and codes, like 3801234567 or 1234567'
+    ),
+});
