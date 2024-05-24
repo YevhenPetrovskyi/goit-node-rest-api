ae9b617b5184504f04c47e0fec39b14780926c9f
YevhenPetrovskyi <71007428+YevhenPetrovskyi@users.noreply.github.com>
2024-05-24 18:41:08 +0300

    add usersRouter

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import contactsRouter from './routes/contactsRouter.js';
+import usersRouter from './routes/usersRouter.js';
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/contacts', contactsRouter);
+app.use('/api/users', usersRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: 'Route not found' });
